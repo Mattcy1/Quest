bfa6cdd3c5e4eea452d7e84e6ac10133714f052d
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-06-02 12:13:21 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "43.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1";
     public const string Name = "Quest++";
 
-    public const string Description = "Adds three types of quests in the top right corner: Bloon Pops, Money Earned, and Quests Completed. Finishing a quest rewards you with monkey money.";
+public const string Description = "Adds three types of quests in the top right corner: Bloon Pops, Money Earned, and Quests Completed. Finishing a quest rewards you with monkey money. Update unlocked! Add a new prestige button as well as some other stuff prestige reset all your quest in exchange for double monkey money (u will get 20 instead of 10 and it stack)";
 
 
     public const string RepoOwner = "Mattcy1";
